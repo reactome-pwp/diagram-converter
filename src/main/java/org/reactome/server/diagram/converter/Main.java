d75b2bb65ceaf41cca49aebb9f81c7f7a89b20b3
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-01-25 11:13:25 +0000

    minor changes

--- src/main/java/org/reactome/server/diagram/converter/Main.java
+++ src/main/java/org/reactome/server/diagram/converter/Main.java
@@ -37,15 +37,11 @@ public class Main {
                         , new FlaggedOption("rel_username", JSAP.STRING_PARSER, "reactome", JSAP.REQUIRED, 'g', "rel_user", "The database username")
                         , new FlaggedOption("rel_password", JSAP.STRING_PARSER, "reactome", JSAP.REQUIRED, 'h', "rel_password", "The password to connect to the database")
 
-                        , new FlaggedOption("output", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'o', "output",
-                        "The directory where the converted files are written to.")
-                        , new FlaggedOption("trivial", JSAP.STRING_PARSER, "trivialchemicals.txt", JSAP.NOT_REQUIRED, 'r', "trivial",
-                        "A file containing the ids and the names of the trivial molecules.")
-                        , new QualifiedSwitch("target", JSAP.STRING_PARSER, "ALL", JSAP.NOT_REQUIRED, 't', "target",
-                        "Target pathways to convert. Use either comma separated IDs, pathways for a given species (e.g. 'Homo sapiens') or 'all' for every pathway").setList(true).setListSeparator(',')
+                        , new FlaggedOption("output", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'o', "output", "The directory where the converted files are written to.")
+                        , new FlaggedOption("trivial", JSAP.STRING_PARSER, "trivialchemicals.txt", JSAP.NOT_REQUIRED, 'r', "trivial", "A file containing the ids and the names of the trivial molecules.")
+                        , new QualifiedSwitch("target", JSAP.STRING_PARSER, "ALL", JSAP.NOT_REQUIRED, 't', "target", "Target pathways to convert. Use either comma separated IDs, pathways for a given species (e.g. 'Homo sapiens') or 'all' for every pathway").setList(true).setListSeparator(',')
 
-                        , new QualifiedSwitch("verbose", JSAP.BOOLEAN_PARSER, null, JSAP.NOT_REQUIRED, 'v', "verbose", "" +
-                        "Requests verbose output.")
+                        , new QualifiedSwitch("verbose", JSAP.BOOLEAN_PARSER, null, JSAP.NOT_REQUIRED, 'v', "verbose", "Requests verbose output.")
                 }
         );
 
