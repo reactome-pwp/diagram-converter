d4b465bdf7f7ad1b072f218aff965b03c0c11b4c
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-10-26 16:33:23 +0100

    cleanup. description from DatabaseObject differed to T114 test

--- src/main/java/org/reactome/server/diagram/converter/layout/output/NodeAttachment.java
+++ src/main/java/org/reactome/server/diagram/converter/layout/output/NodeAttachment.java
@@ -2,20 +2,15 @@ package org.reactome.server.diagram.converter.layout.output;
 
 import org.reactome.server.diagram.converter.layout.input.model.OrgGkRenderRenderableFeature;
 import org.reactome.server.diagram.converter.layout.util.ShapeBuilder;
-import org.reactome.server.graph.domain.model.DatabaseObject;
-import org.reactome.server.graph.service.DatabaseObjectService;
-import org.reactome.server.graph.utils.ReactomeGraphCore;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
- * @author Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
- * @author Antonio Fabregat <fabregat@ebi.ac.uk>
+ * @author Kostas Sidiropoulos (ksidiro@ebi.ac.uk)
+ * @author Antonio Fabregat (fabregat@ebi.ac.uk)
  */
 @XmlRootElement
 public class NodeAttachment {
-
-    private static transient final DatabaseObjectService getDatabaseObject = ReactomeGraphCore.getService(DatabaseObjectService.class);
 
     public Long reactomeId;
     public String label;
@@ -26,16 +21,9 @@ public class NodeAttachment {
     public transient Float relativeY;
 
     public NodeAttachment(OrgGkRenderRenderableFeature obj, Node node) {
+        this.reactomeId = obj.getReactomeId() != null ? obj.getReactomeId().longValue() : null;
         this.label = obj.getLabel();
         this.description = obj.getDescription();
-
-        if (obj.getReactomeId() != null) {
-            this.reactomeId = obj.getReactomeId().longValue();
-            DatabaseObject object =  getDatabaseObject.findById(reactomeId);
-            if(object!=null) {
-                this.description = object.getDisplayName();
-            }
-        }
 
         relativeX = obj.getRelativeX().floatValue();
         relativeY = obj.getRelativeY().floatValue();
@@ -46,7 +34,7 @@ public class NodeAttachment {
         this.shape = ShapeBuilder.createNodeAttachmentBox(boxCentre, label);
     }
 
-    public void translate(Coordinate panning){
-        if(shape!=null) shape.translate(panning);
+    public void translate(Coordinate panning) {
+        if (shape != null) shape.translate(panning);
     }
 }
