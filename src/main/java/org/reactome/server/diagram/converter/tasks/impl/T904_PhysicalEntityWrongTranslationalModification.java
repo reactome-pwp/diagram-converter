98f7cf36b7ea9be48f4d491e412bb5e46fbe6ec9
Antonio Fabregat <fabregat.antonio@gmail.com>
2019-02-21 11:53:58 +0000

    minor changes to the query

--- src/main/java/org/reactome/server/diagram/converter/tasks/impl/T904_PhysicalEntityWrongTranslationalModification.java
+++ src/main/java/org/reactome/server/diagram/converter/tasks/impl/T904_PhysicalEntityWrongTranslationalModification.java
@@ -88,9 +88,9 @@ public class T904_PhysicalEntityWrongTranslationalModification extends AbstractC
                 "WHERE psi.abbreviation IS NULL " +
                 "OPTIONAL MATCH (a)-[:created]->(pe) " +
                 "OPTIONAL MATCH (m)-[:modified]->(pe) " +
-                "WITH DISTINCT pe, tm, psi, a, m " +
-                "ORDER BY m.displayName, a.displayName, pe.stId, tm.displayName " +
-                "RETURN DISTINCT pe.stId + ',\"' + pe.displayName + '\",\"' + tm.displayName + '\",' + tm.schemaClass + ',\"' + psi.displayName + '\",\"' + a.displayName + '\",\"' + m.displayName + '\"' AS line";
+                "WITH DISTINCT pe, tm, psi, CASE WHEN a IS NULL THEN 'null' ELSE a.displayName END AS created, CASE WHEN m IS NULL THEN 'null' ELSE m.displayName END AS modified " +
+                "ORDER BY modified, created, pe.stId, tm.displayName " +
+                "RETURN DISTINCT pe.stId + ',\"' + pe.displayName + '\",\"' + tm.displayName+ '\",' + tm.schemaClass + ',\"' + psi.displayName + '\",\"' + created + '\",\"' + modified + '\"' AS line";
         try {
             Collection<String> res = ads.getCustomQueryResults(String.class, query, params);
             if (res.size() > 0) {
