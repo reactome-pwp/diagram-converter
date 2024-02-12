52d11cd2560af8d71fab6c82746b05f7aadb6e4f
EliotRagueneau <eragueneau@ebi.ac.uk>
2024-02-12 18:08:20 +0000

    :bug: Fix reaction nodes being placed on arrow end

--- src/main/java/org/reactome/server/diagram/converter/layout/output/Edge.java
+++ src/main/java/org/reactome/server/diagram/converter/layout/output/Edge.java
@@ -1,6 +1,7 @@
 package org.reactome.server.diagram.converter.layout.output;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
 
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
@@ -10,5 +11,18 @@ public class Edge extends EdgeCommon {
 
     public Edge(Object obj) {
         super(obj);
+        centerReaction();
+    }
+
+    private void centerReaction() {
+        if (this.segments.size() == 1) {
+            Coordinate p1 = this.segments.get(0).from;
+            Coordinate p2 = this.segments.get(0).to;
+            Coordinate midPoint = new Coordinate((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+            this.points = List.of(p1, midPoint, p2);
+            this.position = midPoint;
+            this.segments = getSegments(this.points);
+            this.points = null;
+        }
     }
 }
